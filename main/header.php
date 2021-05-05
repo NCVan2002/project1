43d1d5cea7c6287094d4afcc245e8e890c68f8d1
nhp1411 <75302956+nhp1411@users.noreply.github.com>
2021-05-06 00:44:00 +0700

    1

--- main/header.php
+++ main/header.php
@@ -1,1 +1,18 @@
-header.php
+<div class="container">
+	<div class="row">
+		<a href="#"><img src="img/logo.jpg"></a>
+		<div class="search-box">
+			<form>
+				<input class="sb-text" type="text" placeholder="Tìm kiếm...">
+				<input class="sb-sbm" type="submit" >
+			</form>
+		</div>
+		<nav>
+			<ul class="main-menu">
+				<li><a href="#">Đăng nhập</a></li>
+				<li><a href="#">Đăng kí</a></li>
+			</ul>
+		</nav>
+	</div>
+		
+</div>
