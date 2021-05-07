1a3631979acd3bc6c55efb0be4d14a81a3f7a58c
nguyencongvan2002 <75302442+nguyencongvan2002@users.noreply.github.com>
2021-05-07 22:00:46 +0700

    1

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,17 @@
+<<<<<<< HEAD
+=======
 <?php session_start(); $connect = new MySQLi("localhost","root","","project"); ?>
+>>>>>>> 0b5a13235d3c63caf9ed2bb6a5684aebc7544d26
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Admin</title>
 	<meta charset="utf-8">
+<<<<<<< HEAD
+</head>
+<body>
+	<?php include"../admin/loginadmin.php"?>
+=======
 	<meta name="viewport" content="width=device-width, initial-scale=2.0">
 </head>
 <body>
@@ -13,5 +21,6 @@
 	}else {
 		include "content/quantri.php";
 	} ?>
+>>>>>>> 0b5a13235d3c63caf9ed2bb6a5684aebc7544d26
 </body>
 </html>
---
+++ main/admin/quantri.php
@@ -0,0 +1,11 @@
+<table width="100%" border="1" cellpadding="0" cellspacing="0">
+	<tr>
+		<td>Hello: <?=$_SESSION['admin']?><br>[<a href="?option=logout">Logout</a>]</td>
+		<td>Trang Quản Trị Admin</td>
+	</tr>
+	<tr>
+		<td>
+			<section>thêm địa điểm:</section>
+		</td>
+	</tr>
+</table>
