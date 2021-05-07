85797721c7bf2c1feb2bbf2849ada9011b18a38a
nguyencongvan2002 <75302442+nguyencongvan2002@users.noreply.github.com>
2021-05-07 11:28:54 +0700

    1

---
+++ admin/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<?php include"../admin/loginadmin.php"?>
+</body>
+</html>
---
+++ main/admin/loginadmin.php
@@ -0,0 +1,40 @@
+<?php
+	if (isset($_POST['username'])) {
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$query = "select * from admin where username='$username' and password='$password'";
+		$result=$connect->query($query);
+		if (mysqli_num_rows($result)==0) {
+			$alert="Sai tên đăng nhập!";
+		}else{
+			$result= mysqli_fetch_array($result);
+			if ($result['status']==0) {
+				$alert="Tài khoản của bạn đã bị khóa";
+			}else{
+				$_SESSION['admin']=$username;
+				header("location: ?option=home");
+			}
+		}
+	}
+?>
+<h1 style="text-align: center;">Đăng nhập tài khoản Amin</h1>
+<section><?=isset($alert)?$alert:""?></section>
+<form method="post" style="margin-left: 600px;">
+	<table style="border: solid 1px;">
+		<tr>
+			<td><label>Username</label></td>
+			<td><input type="text" name="username" required></td>
+		</tr>
+		<tr>
+			<td><label>Password</label></td>
+			<td><input type="text" name="password" required></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="submit" value="Đăng Nhập"></td>
+		</tr>
+	</table>
+</form>
+
+
+
